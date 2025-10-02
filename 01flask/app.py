41223596b420577d12dabee92e4025848c82af04
weiiiliyu <2825024660@qq.com>
2025-10-02 19:23:13 +0800

    learn flask

--- 01flask/app.py
+++ 01flask/app.py
@@ -15,8 +15,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_flask():
-    return 'hello flask'
+    return 'hello flask. success!'
+
+
+app.add_url_rule('/home', 'home', hello_flask)
+print(app.url_map)
+
+
+@app.route('/user')
+@app.route('/user/<page>')
+def list_user(page=100):
+    return f'您好，你是第{page}页用户。'
 
 # v1.0之后的版本，不推荐的写法
 # if __name__ == '__main__':
 #     app.run()
+
